a32634156b87666f13d185cac86403caeac5d3ef
Edwin-96 <51181146+Edwin-96@users.noreply.github.com>
2022-12-03 21:25:00 -0600

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
 import * as React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import anime from 'animejs';
 import './style.css';
 
+const calcBlocks = (amount: number, heightPercent: number) => {
+  let size = window.innerWidth / amount;
+  console.log(window.innerHeight, size);
+  let amountHeight = Math.trunc(
+    (window.innerHeight * (heightPercent / 100)) / size
+  );
+  console.log(amount * amountHeight);
+  return amount * amountHeight;
+};
+
+const setBlocksSize = (size: number) => {
+  document.documentElement.style.setProperty('--block-size', `${size}px`);
+};
+
+const stagger = () => {};
+
+const generateBlocks = (amount: number) => {
+  let blocks: any[] = [];
+  for (let i = 0; i < amount; i++)
+    blocks.push(<div key={i} onClick={stagger} className="block"></div>);
+  return blocks;
+};
+
 export default function App() {
+  const [, setWidth] = useState(0);
+  const amount = 50;
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(window.innerWidth));
+    setBlocksSize(window.innerWidth / amount);
+  });
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <div className="container">{generateBlocks(calcBlocks(amount, 100))}</div>
   );
 }
